4c90448276c5158f08f078134b1f76356b08d548
Vladislav Kutsobin <vladislavkutsobin@gmail.com>
2021-08-02 16:47:35 +0200

    Initial commit

---
+++ src/test/java/com/mayateam/wordsgame/WordsgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayateam.wordsgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordsgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
